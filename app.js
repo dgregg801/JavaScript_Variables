0cf034fade763821a3884f85936f8c3184e900f8
Darin Gregg <daringregg801@gmail.com>
2023-09-05 12:35:06 -0600

    JS variables

--- app.js
+++ app.js
@@ -4,7 +4,40 @@ console.log(
 );
 
 // Exercise 1
+const firstName="Darin"
+var age;
+let lastname;
+console.log(lastname);
 
 // Exercise 2
+firstName;
+lastname="Gregg";
+console.log(firstName);
+age=43;
+console.log(age);
 
 // Exercise 3
+let language="JavaScript";
+let createdYear=1995;
+let isCaseSensitive=true;
+console.log(isCaseSensitive);
+
+// Exercise 4
+let price=19.99;
+let isOnSale=false;
+let salePercentage=15;
+let stock=0;
+let inStock=false;
+let selectedSize="M";
+console.log(price, isOnSale, salePercentage, stock, inStock, selectedSize)
+
+
+
+// Exercise 5
+let title="Name of the Wind";
+let author="Patrick Rothfuss";
+let pageCount=722;
+let bookmark=456;
+let hasRead=true;
+console.log(inStock)
+
